e2e55ca2b854116396b851d81c1dd9976149bdab
Shengyi Xu <54436848+xushengyichn@users.noreply.github.com>
2025-06-08 11:02:14 +0800

    中文词云parrot文件

--- examples/wordcloud_cn.py
+++ examples/wordcloud_cn.py
@@ -26,9 +26,9 @@ from wordcloud import WordCloud, ImageColorGenerator
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-stopwords_path = d + '/wc_cn/stopwords_cn_en.txt'
+stopwords_path = os.path.join(d, "wc_cn/stopwords_cn_en.txt")
 # Chinese fonts must be set
-font_path = d + '/fonts/SourceHanSerif/SourceHanSerifK-Light.otf'
+font_path = os.path.join(d, "fonts/SourceHanSerif/SourceHanSerifK-Light.otf")
 
 # the path to save worldcloud
 imgname1 = d + '/wc_cn/LuXun.jpg'
@@ -36,40 +36,43 @@ imgname2 = d + '/wc_cn/LuXun_colored.jpg'
 # read the mask / color image taken from
 back_coloring = imread(path.join(d, d + '/wc_cn/LuXun_color.jpg'))
 
-# Read the whole text.
-text = open(path.join(d, d + '/wc_cn/CalltoArms.txt')).read()
-
-# if you want use wordCloud,you need it
-# add userdict by add_word()
+text_path = os.path.join(d, "wc_cn/CalltoArms.txt")
 userdict_list = ['阿Ｑ', '孔乙己', '单四嫂子']
+text = open(text_path, encoding="utf-8").read()
 
+def jieba_processing_txt(text, stopwords_path=None, userdict_list=None):
+    if userdict_list:
+        for word in userdict_list:
+            jieba.add_word(word)
 
-# The function for processing text with Jieba
-def jieba_processing_txt(text):
-    for word in userdict_list:
-        jieba.add_word(word)
-
-    mywordlist = []
     seg_list = jieba.cut(text, cut_all=False)
-    liststr = "/ ".join(seg_list)
+    words = [w.strip() for w in seg_list if len(w.strip()) > 1]
 
-    with open(stopwords_path, encoding='utf-8') as f_stop:
-        f_stop_text = f_stop.read()
-        f_stop_seg_list = f_stop_text.splitlines()
+    if stopwords_path:
+        with open(stopwords_path, encoding='utf-8') as f:
+            stopwords = set(f.read().splitlines())
+        words = [w for w in words if w not in stopwords]
 
-    for myword in liststr.split('/'):
-        if not (myword.strip() in f_stop_seg_list) and len(myword.strip()) > 1:
-            mywordlist.append(myword)
-    return ' '.join(mywordlist)
+    return ' '.join(words)
 
+processed_text = jieba_processing_txt(text, stopwords_path=stopwords_path, userdict_list=userdict_list)
 
-wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, mask=back_coloring,
-               max_font_size=100, random_state=42, width=1000, height=860, margin=2,)
-
+wc = WordCloud(
+    font_path=font_path,
+    max_words=5000,
+    mask=back_coloring,
+    max_font_size=50,
+    min_font_size=2,
+    font_step=1,
+    scale=2,
+    prefer_horizontal=1.0,
+    relative_scaling=0,
+    random_state=42,
+    background_color="white"
+)
+wc.generate(processed_text)
 
 if __name__ == '__main__':
-    wc.generate(jieba_processing_txt(text))
-
     image_colors_default = ImageColorGenerator(back_coloring)
 
     plt.figure()
