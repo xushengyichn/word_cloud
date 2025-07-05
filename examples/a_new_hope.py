de193e1ea9b50c91f58bd3176e9567973051ffd9
Shengyi Xu <54436848+xushengyichn@users.noreply.github.com>
2025-07-05 16:48:32 +0800

    新增黑白版本

--- examples/a_new_hope.py
+++ examples/a_new_hope.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import os
 import random
 
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
 def grey_color_func(word, font_size, position, orientation, random_state=None,
@@ -26,7 +26,10 @@ d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
 # read the mask image taken from
 # http://www.stencilry.org/stencils/movies/star%20wars/storm-trooper.gif
-mask = np.array(Image.open("/Users/shengyixu/Library/CloudStorage/OneDrive-Personal/NAS云同步/Drive/社会工作/2025年07月05日短视频竞赛/中企联合大厦简笔画1.png"))
+mask = np.array(Image.open("/Users/shengyixu/Library/CloudStorage/OneDrive-Personal/NAS云同步/Drive/社会工作/2025年07月05日短视频竞赛/金汇社区卫生服务中心2.png"))
+
+# 读取颜色图片（可以是彩色版本的mask图片）
+color_image = np.array(Image.open("/Users/shengyixu/Library/CloudStorage/OneDrive-Personal/NAS云同步/Drive/社会工作/2025年07月05日短视频竞赛/金汇社区卫生服务中心3.png"))
 
 # 导入词频数据
 from word_frequencies import word_frequencies
@@ -54,18 +57,35 @@ wc = WordCloud(
 # 直接使用词频生成词云，而不是处理文本
 wc.generate_from_frequencies(word_frequencies)
 
-# wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
-            #    random_state=1, background_color='white').generate(text)
+# 创建从图片中获取颜色的生成器
+image_colors = ImageColorGenerator(color_image)
 
 # store default colored image
 default_colors = wc.to_array()
-plt.title("Custom colors")
-plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3),
-           interpolation="bilinear")
-wc.to_file("a_new_hope.png")
-plt.axis("off")
-plt.figure()
-plt.title("Default colors")
-plt.imshow(default_colors, interpolation="bilinear")
-plt.axis("off")
+# 显示三种不同的效果
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+# 1. 默认颜色
+axes[0].set_title("Default colors")
+axes[0].imshow(default_colors, interpolation="bilinear")
+axes[0].axis("off")
+
+# 2. 灰度颜色
+axes[1].set_title("Grey colors")
+axes[1].imshow(wc.recolor(color_func=grey_color_func, random_state=3),
+               interpolation="bilinear")
+axes[1].axis("off")
+
+# 保存黑白的版本
+wc.to_file("a_new_hope_black_white.png")
+
+# 3. 从图片获取的颜色
+axes[2].set_title("Image colors")
+axes[2].imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
+axes[2].axis("off")
+
+plt.tight_layout()
 plt.show()
+
+# 保存两个版本
+wc.recolor(color_func=image_colors).to_file("a_new_hope_image_colors.png")
