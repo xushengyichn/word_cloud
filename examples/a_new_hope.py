11783e1724a38ff07b60f977dedeadf9c8250330
Shengyi Xu <54436848+xushengyichn@users.noreply.github.com>
2025-07-05 16:21:40 +0800

    update

--- examples/a_new_hope.py
+++ examples/a_new_hope.py
@@ -18,7 +18,7 @@ from wordcloud import WordCloud, STOPWORDS
 
 def grey_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(20, 60)
+    return "hsl(0, 0%%, %d%%)" % random.randint(1, 20)
 
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
@@ -26,16 +26,10 @@ d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
 # read the mask image taken from
 # http://www.stencilry.org/stencils/movies/star%20wars/storm-trooper.gif
-mask = np.array(Image.open("/Users/shengyixu/Library/CloudStorage/OneDrive-Personal/NAS云同步/Drive/社会工作/2025年07月05日短视频竞赛/金汇社区卫生服务中心2.png"))
+mask = np.array(Image.open("/Users/shengyixu/Library/CloudStorage/OneDrive-Personal/NAS云同步/Drive/社会工作/2025年07月05日短视频竞赛/中企联合大厦简笔画1.png"))
 
-# movie script of "a new hope"
-# http://www.imsdb.com/scripts/Star-Wars-A-New-Hope.html
-# May the lawyers deem this fair use.
-text = open(path.join(d, 'a_new_hope.txt')).read()
-
-# pre-processing the text a little bit
-text = text.replace("HAN", "Han")
-text = text.replace("LUKE'S", "Luke")
+# 导入词频数据
+from word_frequencies import word_frequencies
 
 # adding movie script specific stopwords
 stopwords = set(STOPWORDS)
@@ -49,14 +43,16 @@ wc = WordCloud(
     margin=10,
     random_state=1,
     background_color='white',
-    # max_font_size=100,         # 减小最大字体大小
-    # min_font_size=10          # 设置最小字体大小
-    # relative_scaling=0.5,     # 增加相对缩放，让词频差异更明显
+    font_path='/System/Library/Fonts/STHeiti Medium.ttc',  # 使用系统中文字体
+    max_font_size=50,         # 减小最大字体大小
+    min_font_size=1,          # 设置最小字体大小
+    # relative_scaling=0.1,     # 增加相对缩放，让词频差异更明显
     # width=800,                # 设置宽度
     # height=600,               # 设置高度
     # prefer_horizontal=0.7     # 70%的词水平排列，30%垂直排列
 )
-wc.generate(text)
+# 直接使用词频生成词云，而不是处理文本
+wc.generate_from_frequencies(word_frequencies)
 
 # wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
             #    random_state=1, background_color='white').generate(text)
